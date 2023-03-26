16a208f7c6373fcbc41159804b3d7cec23f80372
Drew <drewdavis888@gmail.com>
2023-03-26 12:33:50 -0600

    add notes

--- src/decorators/Model.ts
+++ src/decorators/Model.ts
@@ -1,6 +1,17 @@
 import { MODEL_TYPE } from "../symbols";
 import ModelMixin from "../mixins/Model";
 
+// TODO can I simplify by adding a prototype method instead of using mixin
+// https://javascript.plainenglish.io/ts-5-0-beta-new-decorators-are-here-5b13a383e4ad
+
+// function Greeter(value, context) {
+//   if (context.kind === "class") {
+//     value.prototype.greet = function () {
+//       console.log("Hello Bytefer!");
+//     };
+//   }
+// }
+
 type GConstructor<T = {}> = new (...args: any[]) => T;
 // TODO try to make stricter
 // type Modelable = GConstructor<{ name: string }>;
