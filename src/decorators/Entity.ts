910abd04ecd72a7bc401167eff22c678d82bba02
Drew <drewdavis888@gmail.com>
2023-07-11 00:57:02 -0600

    cleanup

--- src/decorators/Entity.ts
+++ src/decorators/Entity.ts
@@ -1,8 +1,10 @@
 import Metadata from "../metadata";
 
-function Entity(target: Function, _context: ClassDecoratorContext) {
-  const tableName = Object.getPrototypeOf(target).name;
-  Metadata.addEntity(target.name, tableName);
+function Entity(target: Function, context: ClassDecoratorContext) {
+  if (context.kind === "class") {
+    const tableName = Object.getPrototypeOf(target).name;
+    Metadata.addEntity(target.name, tableName);
+  }
 }
 
 export default Entity;
