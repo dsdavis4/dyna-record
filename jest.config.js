b5a1802d03247fb431d3ba0026803daf4cbc9e3e
Drew <drewdavis888@gmail.com>
2024-03-22 00:04:44 -0600

    Adjust jest thresholds

--- jest.config.js
+++ jest.config.js
@@ -12,10 +12,10 @@ module.exports = {
   coveragePathIgnorePatterns: ["README.md", "/node_modules/", "dist"],
   coverageThreshold: {
     global: {
-      branches: 90,
-      functions: 90,
-      lines: 90,
-      statements: 90
+      branches: 85,
+      functions: 85,
+      lines: 85,
+      statements: 85
     }
   }
 };
