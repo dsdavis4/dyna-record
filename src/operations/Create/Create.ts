dc7ac3e8cf1a917125c44290bb9e473c5138b27d
Drew <drewdavis888@gmail.com>
2025-01-08 19:15:50 -0700

    add note

--- src/operations/Create/Create.ts
+++ src/operations/Create/Create.ts
@@ -281,6 +281,7 @@ class Create<T extends DynaRecord> extends OperationBase<T> {
   // TODO Do I really need the BelongsToLinks prefetched in order to get build these transactions?
   // I think I can build soley from metadata...
   // If so I need to remove the get code from here as well tests
+  // TODO this block does not appear to have unit tests
 
   /**
    * For each related entity referenced by a "BelongsTo" relationship, insert a denormalized copy of that entity
