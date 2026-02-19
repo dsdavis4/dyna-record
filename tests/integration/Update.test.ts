71c40a33a3702f8936033dac4044d1e34c190e16
Drew <drewdavis888@gmail.com>
2026-02-19 08:28:46 -0700

    add note

--- tests/integration/Update.test.ts
+++ tests/integration/Update.test.ts
@@ -140,6 +140,12 @@ class MockInformation extends MockTable {
 // TODO make sure there is support for lists, that dont have to be nested in objects
 // TODO add type unit tests for replacing an object, and that on update even nullable fields are respected
 
+// TODO not this page but make sure readme docs are good
+
+// TODO for both findById and query test that the serialzied object has the correct types
+
+// TODO make sure there are tests for runtime validations for write operations
+
 describe("Update", () => {
   beforeAll(() => {
     jest.useFakeTimers();
